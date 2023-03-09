2f5f7b118b0dce69790aa30b149d31123c15113e
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-03-09 18:37:03 +0300

    adding two int addToNumbers

---
+++ addToNumbers.java
@@ -0,0 +1,8 @@
+public class addToNumbers{
+    public static void main(String[] args){
+        int num1=29;
+        int num2=78;
+        int sum =num1 + num2;
+        System.out.println("the sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
