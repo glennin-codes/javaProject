d3eda0c9b1a6e05483095c9e918ec05f4767f161
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-03-09 18:51:50 +0300

    narrowing or type catsing

---
+++ narrowing.java
@@ -0,0 +1,9 @@
+public class narrowing{
+    public static void main(String[] args){
+        double a=10;
+        int b=(int)a;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
