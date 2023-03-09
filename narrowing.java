063ceddf8984f759c2f2cc387626b05bfc401944
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-03-09 19:15:02 +0300

    lowerType

--- narrowing.java
+++ narrowing.java
@@ -1,9 +1,15 @@
 public class narrowing{
+    private static void add(){ 
+        byte a =10;
+        byte b = 10;
+        byte c= (byte)(a+b);
+        System.out.println(c);
+    }
     public static void main(String[] args){
         double a=10;
         int b=(int)a;
         System.out.println(a);
         System.out.println(b);
-
+       add();
     }
 }
