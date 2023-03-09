001682eea65362c5f9adfd0f7e49150218df734e
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-03-09 19:04:51 +0300

    java overflow

---
+++ lowerType.java
@@ -0,0 +1,8 @@
+public class lowerType {
+    public static void main (String[] args){
+        byte a =(byte) 348;
+        byte b= (byte)a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
