463db3f635bc67f9a501bb39c4d5cc5ebd87f72b
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-03-13 16:14:34 +0300

    sweetJava

--- forEach.java
+++ forEach.java
@@ -1,5 +1,6 @@
 public class forEach {
     public static void main (String[] args){
+
         int [] numbers={1,2,3,4,5,567,6,7,0};
         for(int number:numbers){
             System.out.println(number);
---
+++ whileLoop.java
@@ -0,0 +1,11 @@
+public class whileLoop {
+    public static void main(String[] args){
+     int i=0;
+     System.out.println("even numbers in 10/n");
+     while(i<=23){
+        System.out.println(i);
+        i=i+2;
+     }
+
+    }
+}
