ee12e798ccb8166f61ff3f1d2b2627755576761c
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-03-13 15:36:51 +0300

    how swwet is java

---
+++ calculation.java
@@ -0,0 +1,9 @@
+public class calculation {
+    public static void main(String[] args){
+  for(int i=0;i<=10;i++){
+   int sum =0;
+int total=sum + i;
+System.out.println(total);
+  }
+    }
+}
---
+++ forEach.java
@@ -0,0 +1,8 @@
+public class forEach {
+    public static void main (String[] args){
+        int [] numbers={1,2,3,4,5,567,6,7,0};
+        for(int number:numbers){
+            System.out.println(number);
+        }
+    }
+}
